4e8bf9bf590b6faaf99d40851cdf19963c51e7c5
AnYack test <missis.anyack@yandex.ru>
2023-08-14 09:32:52 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,19 @@ public class Main {
         int balance = 100;
         int payment = 1100;
 
-        int bonus_payment_threshold = 1000;
-        int bonus_delim = 100;
-        int total_bonuses = 0;
+        int bonusPaymentThreshold = 1000;
+        int bonusDelim = 100;
+        int totalBonuses = 0;
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
-        if (payment > bonus_payment_threshold) {
-            total_bonuses = payment / bonus_delim;
+        if (payment > bonusPaymentThreshold) {
+            totalBonuses = payment / bonusDelim;
         }
-        balance += payment + total_bonuses;
+        balance += payment + totalBonuses;
         System.out.println(balance);
-        System.out.println(total_bonuses);
+        System.out.println(totalBonuses);
     }
 }
